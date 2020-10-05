1b835035cc09f2fe9600629a228ca19deb25e550
Natalya Ryabaya <natalir.80@gmail.com>
2020-10-05 22:01:27 +0300

    Добавлены файлы

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 1_000_000_000;
+        int transfer = 1_250_000_000;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
